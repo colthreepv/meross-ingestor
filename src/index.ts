3bc9a0b1b04e1ea95f07bba88151b616c0a99cf9
colthreepv <valerio.coltre@gmail.com>
2020-03-14 02:25:44 +0100

    using the modified lib

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,10 @@ import MerossCloud, {
   GetControlElectricityResponse, GetControlPowerConsumptionXResponse, MerossCloudDevice,
 } from 'meross-cloud'
 
-// import { promisify } from 'util'
-
 const options = {
   email: process.env.MEROSS_EMAIL!,
   password: process.env.MEROSS_PASSWORD!,
 }
-
-const meross = new MerossCloud(options)
 
 const readTimeStamp = (time) => new Date(time * 1000)
 const readConsumptionX = (res: GetControlPowerConsumptionXResponse) => {
@@ -34,46 +30,37 @@ const readElectricity = (res: GetControlElectricityResponse) => {
   }
 }
 
-meross.on('deviceInitialized', (deviceId, deviceDef, device) => {
-  console.log('New device ' + deviceId + ': ' + JSON.stringify(deviceDef))
+async function printConsumption (device: MerossCloudDevice) {
+  const [consumption, electricity] = await Promise.all([
+    device.getControlPowerConsumptionX(),
+    device.getControlElectricity(),
+  ])
+  console.log(device.dev.devName, 'electricity', readElectricity(electricity))
+  console.log(device.dev.devName, 'consumption', readConsumptionX(consumption))
+}
 
-  function printConsumption (device: MerossCloudDevice) {
-    // console.log('device', device)
-    // device.getControlPowerConsumptionX((err, res) => console.log('Consumption', readConsumptionX(res)))
-    device.getControlElectricity((err, res) => console.log(deviceDef.devName, 'electricity', readElectricity(res)))
-    console.log('--------------')
-  }
+async function main () {
+  const meross = new MerossCloud(options)
+  const deviceList = await meross.connect()
+  console.log('meross connected')
+  // const firstDevice = deviceList[0]
+  // printConsumption(firstDevice)
 
-  device.on('connected', () => {
-    // device.getSystemAbilities((err, res) => console.log(deviceDef, 'Abilities: ' + JSON.stringify(res, null, 2)))
+  // firstDevice.on('connected', () => printConsumption(firstDevice))
+  // setInterval(() => printConsumption(firstDevice), 10 * 1000)
 
-    printConsumption(device)
-    setInterval(() => printConsumption(device), 10 * 1000)
-  })
-})
+  deviceList.forEach(printConsumption)
+  setInterval(() => deviceList.forEach(printConsumption), 10 * 1000)
+}
 
-meross.connect((error) => {
-  console.log('connect error: ' + error)
-})
+try {
+  main()
+} catch (err) {
+  console.error('Error happened', err)
+  process.exit(1)
+}
 
-// meross.on('connected', (deviceId) => {
-//   console.log(deviceId + ' connected');
-// });
-
-// meross.on('close', (deviceId, error) => {
-//   console.log(deviceId + ' closed: ' + error);
-// });
-
-// meross.on('error', (deviceId, error) => {
-//   console.log(deviceId + ' error: ' + error);
-// });
-
-// meross.on('reconnect', (deviceId) => {
-//   console.log(deviceId + ' reconnected');
-// });
-
-// meross.on('data', (deviceId, payload) => {
-//   console.log(deviceId + ' data: ' + JSON.stringify(payload));
-// });
-
-
+process.on('unhandledRejection', function(reason, p){
+  console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
+  // application specific logging here
+});
